3e137c806202a4cfb3005b1f13b2634a3c989edb
francksefu <francksefu1998@gmail.com>
2023-07-20 21:28:42 +0200

    Add content for main.rb and enumerable.mrb

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative 'my_list'
+require_relative 'enumerable'
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # Output: true
+puts(list.all? { |e| e > 5 }) # Output: false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # Output: true
+puts(list.any? { |e| e == 5 }) # Output: false
+
+# Test #filter
+puts(list.filter(&:even?)) # Output: [2, 4]
